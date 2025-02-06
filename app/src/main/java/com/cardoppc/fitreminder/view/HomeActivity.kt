520bcd47082bcfad35836e10a10daa5d02871523
juliancr14 <juliancardonarojas3@gmail.com>
2025-02-06 11:53:35 -0500

    Adding the view to home activity that was propuse in mockup

--- app/src/main/java/com/cardoppc/fitreminder/view/HomeActivity.kt
+++ app/src/main/java/com/cardoppc/fitreminder/view/HomeActivity.kt
@@ -2,22 +2,35 @@ package com.cardoppc.fitreminder.view
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.cardoppc.fitreminder.R
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val menuIcon = findViewById<View>(R.id.menu_icon)
         val bundle = intent.extras
         val email = bundle?.getString("email") ?: ""
         val provider = bundle?.getString("provider") ?: ""
+
+        menuIcon.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(androidx.core.view.GravityCompat.START)) {
+                drawerLayout.closeDrawer(androidx.core.view.GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(androidx.core.view.GravityCompat.START)
+            }
+        }
 
         setup(email, provider)
 
@@ -28,20 +41,20 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun setup(email: String, provider: String) {
-        title = "Inicio"
+        //title = "Inicio"
 
-        val txtEmail = findViewById<TextView>(R.id.txtVEmail)
-        val txtVProveedor = findViewById<TextView>(R.id.txtVProveedor)
-        val btnCerrarSesion = findViewById<Button>(R.id.btnCerrarSesion)
+        //val txtEmail = findViewById<TextView>(R.id.txtVEmail)
+        //val txtVProveedor = findViewById<TextView>(R.id.txtVProveedor)
+        //val btnCerrarSesion = findViewById<Button>(R.id.btnCerrarSesion)
 
-        txtEmail.text = email
-        txtVProveedor.text = provider
+        //txtEmail.text = email
+        //txtVProveedor.text = provider
 
-        btnCerrarSesion.setOnClickListener {
-            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
-            prefs.clear()
-            prefs.apply()
-            onBackPressedDispatcher.onBackPressed()
-        }
+        //btnCerrarSesion.setOnClickListener {
+        //    val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        //    prefs.clear()
+        //    prefs.apply()
+        //    onBackPressedDispatcher.onBackPressed()
+        //}
     }
 }
